d7ecaf981c3e134d2fc96b9bd53e43f74bb76f05
JE-Pankaj-Yadav <py0354895@gmail.com>
2025-07-28 22:07:19 +0530

    Some Basic Problem

---
+++ Problem_No1.sql
@@ -0,0 +1,6 @@
+--Problem No. 1:  Query all columns for all American cities in the CITY table with populations larger than 100000. The CountryCode for America is USA.
+Select *
+From city
+Where
+  countrycode = 'USA'
+  AND population > 100000;
